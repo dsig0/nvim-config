9ced7405c3b98e7d63dd79337d0a599efed5bb1a
dsig0 <75738471+dsig0@users.noreply.github.com>
2025-11-09 09:30:05 +0530

    u

--- flake.nix
+++ flake.nix
@@ -9,12 +9,14 @@
     homeManagerModules.default = { config, pkgs, ... }: {
       programs.neovim.enable = true;
 
-      # ✅ This makes your Neovim config appear at ~/.config/nvim
-        home.file.".config/nvim" = {
-    source = self;
-    recursive = true;
-  };
+      home.file.".config/nvim" = {
+        source = builtins.filterSource
+          (path: type:
+            !(builtins.elem (baseNameOf path) [ "flake.nix" "flake.lock" "readme.md" ]))
+          self;
+        recursive = true;
+      };
     };
-};
+  };
 }
 
