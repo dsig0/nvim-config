ef0d2a0103a72a929b51263db1b5d474c495b1d2
dsig0 <75738471+dsig0@users.noreply.github.com>
2025-11-09 08:37:02 +0530

    u

--- flake.nix
+++ flake.nix
@@ -5,29 +5,15 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
   };
 
- outputs = { self, nixpkgs, ... }:
-    let
-      system = "x86_64-linux";
-      pkgs = import nixpkgs { inherit system; };
-    in {
-      homeManagerModules.default = { config, pkgs, ... }: {
-        programs.neovim = {
-          enable = true;
-          package = pkgs.neovim;
-          # ✅ This is the correct, supported option
-          extraConfig = ''
-            lua << EOF
-            vim.cmd("source ${self}/init.lua")
-            EOF
-          '';
-        };
-      };
+  outputs = { self, nixpkgs, ... }: {
+    homeManagerModules.default = { config, pkgs, ... }: {
+      programs.neovim.enable = true;
 
-      # optional export
-      packages.${system}.nvim-config-src = pkgs.runCommand "nvim-config-src" {} ''
-        mkdir -p $out
-        cp -r ${self}/* $out
-      '';
+      # ✅ This makes your Neovim config appear at ~/.config/nvim
+        home.file.".config/nvim" = {
+    source = self;
+    recursive = true;
+  };
     };
 }
 
